84450744a22ad4feac265fbd566c4e9b87c05879
simone <simonegauli@gmail.com>
2023-08-06 23:13:39 +0200

    fix wrong error type

--- src/cor-route/catch-error.ts
+++ src/cor-route/catch-error.ts
@@ -6,6 +6,9 @@ export const errToString = (err: unknown, ...args: string[]) => {
   }
   if (err instanceof Error) return err.message;
   if (typeof err === 'string') return err;
-  if (typeof err === 'object' && err !== null && 'description' in err) return err['description']; // TELEGRAM ERROR LIKE
+  if (typeof err === 'object' && err !== null && 'description' in err) {
+    const error = err['description'];
+    if (typeof error === 'string') return error;
+  }
   return 'API error';
 };
