09771cce23986675dc221ce673d57c8f3f4fd5fc
simone <simonegauli@gmail.com>
2023-12-07 20:40:43 +0100

    fix callback type

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import { getGptCommand } from './lib/gpt-command.js';
 
 type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N ? Acc[number] : Enumerate<N, [...Acc, Acc['length']]>;
 
-type Callback = () => Promise<void> | (() => void);
+export type Callback = (() => Promise<void>) | (() => void);
 
 export type Range<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>;
 
