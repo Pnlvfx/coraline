456f0fc524050561fbc9106ba2c9c1e63ef8ace3
simone <simonegauli@gmail.com>
2024-01-07 16:19:03 +0100

    aa

--- src/lib/sign.ts
+++
@@ -1,7 +0,0 @@
-export const signServer = (name: string, author: string) => {
-  return `=================================\n';
-  '| ${name}                       |\n';
-  '|                               |\n';
-  '| by ${author}                  |\n';
-  '=================================\n\n\n`;
-};
