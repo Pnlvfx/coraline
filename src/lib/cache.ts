fe506bdd9ae470ecf68ac79e9b504de6ec2b9617
simone <simonegauli@gmail.com>
2024-02-19 21:35:06 +0100

    improve cache

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,116 +1,64 @@
 import path from 'node:path';
-import { isProduction, readJSON, rm, saveFile, use } from './init.js';
+import { readJSON, rm, saveFile, use } from './init.js';
+import { Callback } from '../types/shared.js';
+const cacheDir = use('cache');
 
-interface Cache {
+interface Cache<T> {
   timestamp: number;
-  data: unknown;
+  data: T;
   isStored: boolean;
   customId?: string;
 }
 
-type CacheInfo = Record<string, { isValid: boolean }>;
+const caches: Partial<Record<string, Cache<unknown>>> = {};
 
-const caches: Partial<Record<string, Cache>> = {};
-const cacheInfo: Partial<CacheInfo> = {};
-let initCacheDir: string | undefined;
-let initStoredCacheNamePath: string | undefined;
-
-const getCache = async (name: string) => {
-  if (!initStoredCacheNamePath) throw new Error('Invalid cache, please report it');
-  if (cacheInfo[name]) return cacheInfo[name];
+const getStored = async <T>(name: string) => {
   try {
-    const info = await readJSON<typeof cacheInfo>(initStoredCacheNamePath);
-    if (!info[name]) return;
-    return info[name];
+    const file = path.join(cacheDir, `${name}.json`);
+    return readJSON<Cache<T>>(file);
   } catch {
-    return { isValid: false };
+    return;
   }
 };
 
-const getStored = async (name: string) => {
-  const info = await getCache(name);
-  if (!info || !info?.isValid || !initCacheDir) return;
-  const file = path.join(initCacheDir, `${name}.json`);
-  return readJSON<Cache>(file);
-};
-
-const store = async (cache: Cache, name: string) => {
-  if (!initCacheDir || !initStoredCacheNamePath) throw new Error('Invalid cache store, please report it!');
-  const file = path.join(initCacheDir, `${name}.json`);
+const store = async <T>(cache: Cache<T>, name: string) => {
+  const file = path.join(cacheDir, `${name}.json`);
   await saveFile(file, JSON.stringify(cache));
-  cacheInfo[name] = { isValid: true };
-  await saveFile(initStoredCacheNamePath, JSON.stringify(cacheInfo));
 };
 
-const cachedRequest = async <T>(
-  name: string,
-  callback: () => Promise<T>,
-  options?: { customId?: string; expires?: number; store?: boolean },
-  // eslint-disable-next-line sonarjs/cognitive-complexity
-): Promise<T> => {
-  if (!initCacheDir) {
-    initCacheDir = use('cache');
-    initStoredCacheNamePath = path.join(initCacheDir, 'cache-info.json');
-  }
+const useCache = async <T>(name: string, callback: Callback<T>, options?: { customId?: string; expires?: number; store?: boolean }): Promise<T> => {
+  const saved = options?.store ? await getStored(name) : caches[name];
   const currentTime = Date.now();
-  let cache = options?.store ? await getStored(name) : caches[name];
 
-  if (options?.customId && options.customId !== cache?.customId) {
-    const data = await callback();
-    cache = {
-      data,
+  if (!saved || options?.customId !== saved.customId || (options?.expires && currentTime - saved.timestamp > options.expires)) {
+    const newCache = {
+      data: await callback(),
       timestamp: currentTime,
-      isStored: options.store || false,
-      customId: options.customId,
+      isStored: options?.store || false,
     };
-    caches[name] = cache;
+    caches[name] = newCache;
 
     if (options?.store) {
-      await store(cache, name);
+      await store(newCache, name);
     }
-
-    return data;
+    return newCache.data;
   }
 
-  if (cache) {
-    if (options?.expires) {
-      if (currentTime - cache.timestamp < options.expires) {
-        // eslint-disable-next-line no-console
-        if (!isProduction) console.log('Returned from cache, expires in:', options.expires - (currentTime - cache.timestamp));
-        return cache.data as T;
-      }
-    } else {
-      return cache.data as T;
-    }
-  }
-
-  const data = await callback();
-  cache = {
-    data,
-    timestamp: currentTime,
-    isStored: options?.store || false,
-  };
-  caches[name] = cache;
-
-  if (options?.store) {
-    await store(cache, name);
-  }
-  return data;
-};
-
-const clearAllCache = () => {
-  if (!initCacheDir) {
-    initCacheDir = use('cache');
-  }
-  if (!initStoredCacheNamePath) {
-    initStoredCacheNamePath = path.join(initCacheDir, 'cache-info.json');
-  }
-  return rm([initCacheDir, initStoredCacheNamePath]);
+  return saved.data as T;
 };
 
 const cache = {
-  add: cachedRequest,
-  clearAll: clearAllCache,
+  use: useCache,
+  clear: async (name: string) => {
+    await rm(path.join(cacheDir, `${name}.json`));
+    delete caches[name];
+  },
+  clearAll: () => {
+    Object.entries(caches).map(([key]) => {
+      delete caches[key];
+    });
+    return rm(cacheDir);
+  },
 };
 
 export default cache;
