eb2c5fd32d16ee49fc7ee59e282cd118e92d368d
simone <simonegauli@gmail.com>
2024-07-17 21:00:17 +0200

    fix benchmark fail

--- src/lib/benchmark.ts
+++ src/lib/benchmark.ts
@@ -3,7 +3,12 @@ import { Callback } from 'coraline-client';
 
 export const benchmark = async <T>(name: string, callback: Callback<T>) => {
   console.time(name);
-  const maybe = await callback();
-  console.timeEnd(name);
-  return maybe;
+  try {
+    const maybe = await callback();
+    console.timeEnd(name);
+    return maybe;
+  } catch (err) {
+    console.timeEnd(name);
+    throw err;
+  }
 };
