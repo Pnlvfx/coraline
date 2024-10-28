e835c1e244a256f31d86b8c1d3e9ab12c36c6809
simone <simonegauli@gmail.com>
2024-10-28 21:17:18 +0100

    fix wrong user agent

--- src/lib/user-agent.ts
+++ src/lib/user-agent.ts
@@ -1,21 +1,19 @@
 import os from 'node:os';
 
+// GET THE LATEST FROM:
+// https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome
+
 export const getUserAgent = () => {
   const system = os.platform();
   switch (system) {
     case 'darwin': {
-      return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36';
+      return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36';
     }
     case 'linux': {
-      return `x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36`;
-    }
-    case 'win32': {
-      const winVersion = os.release().split('.').at(0);
-      if (!winVersion) throw new Error('Something went wrong while getting user agent.');
-      return `Mozilla/5.0 (Windows NT ${winVersion}; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36`;
+      return `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36`;
     }
     default: {
-      return `Mozilla/5.0 (compatible; Node.js/${process.version}; ${process.platform} ${process.arch})`;
+      return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36';
     }
   }
 };
