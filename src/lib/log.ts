65d15e5efe7019504265f95960498d4dfa9c88cc
simone <simonegauli@gmail.com>
2024-11-15 02:26:57 +0100

    fix log to file

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,14 +1,19 @@
 import { inspect } from 'node:util';
 import { isProduction } from './shared.js';
 import { temporaryFile } from './tempy.js';
+import fs from 'node:fs/promises';
 
 export const logToFile = async (data: unknown) => {
   if (isProduction) throw new Error('Do not use coraline.log in production as it is used only for debugging purposes.');
   let file: string | undefined;
   if (typeof data === 'string' || typeof data === 'number') {
     file = await temporaryFile({ extension: '' });
+    await fs.writeFile(file, data.toString());
   } else if (Array.isArray(data) || (typeof data === 'object' && data !== null)) {
     file = await temporaryFile({ extension: 'json' });
+    await fs.writeFile(file, JSON.stringify(data));
+  } else {
+    throw new Error(`Unsupported data for logging: ${JSON.stringify(data)}`);
   }
   // eslint-disable-next-line no-console
   console.log(file);
